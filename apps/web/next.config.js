0c29b434bbc974f3786b71aa4d2936bf4293deaf
lyanbrito <lyanbrito613@gmail.com>
2025-11-09 02:56:19 -0300

    test: adding module-federation dependencie for nesting pages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,23 @@
+import { NextFederationPlugin } from '@module-federation/nextjs-mf';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack(config, options) {
+        config.plugins.push(
+            new NextFederationPlugin({
+                name: 'web',
+                filename: 'static/chunks/remoteEntry.js',
+                exposes: {
+                    './App': './app/page.tsx',
+                },
+                shared: {
+                    react: { singleton: true },
+                    'react-dom': { singleton: true },
+                },
+            })
+        );
+        return config;
+    },
+};
 
 export default nextConfig;
